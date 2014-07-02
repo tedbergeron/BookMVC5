9ae4ea37f27f6ae2c691ff2a7ea9737d1e405ddb
tedbergeron <tbergeron@cisoregon.org>
2014-07-02 13:21:48 -0700

    validation and styling

--- Chapter 02/Partyinvites/Controllers/HomeController.cs
+++ Chapter 02/Partyinvites/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Partyinvites.Models;
 
 namespace Partyinvites.Controllers
 {
@@ -17,9 +18,26 @@ namespace Partyinvites.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult RsvpForm() 
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) 
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+            
         }
     }
 }

c7509f2457b76ce1a555748a702e8217c6378d7a
tedbergeron <tbergeron@cisoregon.org>
2014-08-01 14:07:07 -0700

    ch6 Unit Testing

---
+++ EssentialTools/EssentialTools/Models/MinumumDiscountHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class MinumumDiscountHelper : IDiscountHelper 
+    {
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            //throw new NotImplementedException();
+            if (totalParam < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else if (totalParam > 100)
+            {
+                return totalParam * 0.9M;
+            } 
+            //else if (totalParam > 10 && totalParam <= 100) 
+            else if (totalParam >= 10 && totalParam <= 100) 
+            {
+                return totalParam - 5;
+            }
+            else 
+            {
+                return totalParam;
+            }
+                
+            
+        }
+    }
+}

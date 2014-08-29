6482308a31f4e5bbc5ac5c3323888a321aa72bdf
tedbergeron <tbergeron@cisoregon.org>
2014-08-29 13:26:29 -0700

    SportsStore ch9 cart2
    does not compile

--- SportsStore/SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/CartController.cs
@@ -74,5 +74,10 @@ namespace SportsStore.WebUI.Controllers
             return PartialView(cart);
         }
 
+        public ViewResult Checkout()
+        {
+            //return View(new ShippingDetails());
+        }
+
 	}
 }

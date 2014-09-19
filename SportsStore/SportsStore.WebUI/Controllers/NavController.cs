142585c851283bdc2334cd51748096f9daff75d3
tedbergeron <tbergeron@cisoregon.org>
2014-09-19 13:37:08 -0700

    SportsStore ch 10 mobile views
    note: removed Views\Nav\Menu.cshtml & MenuHorizontal.cshtml by
    consolidating the 2 views into one FlexMenu.cshtml

--- SportsStore/SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/NavController.cs
@@ -17,7 +17,7 @@ namespace SportsStore.WebUI.Controllers
             repository = repo;
         }
 
-        public PartialViewResult Menu(string category = null, bool horizontalLayout = false)
+        public PartialViewResult Menu(string category = null)
         {
             ViewBag.SelectedCategory = category;
 
@@ -26,9 +26,10 @@ namespace SportsStore.WebUI.Controllers
                 .Distinct()
                 .OrderBy(x => x);
 
-            string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
+            //string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
 
-            return PartialView(viewName, categories);
+            //return PartialView(viewName, categories);
+            return PartialView("FlexMenu", categories);
         }
 	}
 }

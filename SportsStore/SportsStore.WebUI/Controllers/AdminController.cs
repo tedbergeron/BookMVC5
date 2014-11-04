c8fc57959d38cff19fcce5750ccff5dc6d844cf1
tedbergeron <tbergeron@cisoregon.org>
2014-11-04 13:41:11 -0800

    SportsStore ch11 admin-edit-validation
    adds updated jQuery and unobtrusive validation

--- SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -47,5 +47,10 @@ namespace SportsStore.WebUI.Controllers
             }
         }
 
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
+        }
+
 	}
 }
